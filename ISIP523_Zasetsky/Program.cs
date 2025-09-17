831fa5e8293fc4277a89459e9f85995e9d4044bf
Reflex414 <9629970659rus@mail.ru>
2025-09-17 13:37:38 +0300

    Написал две функции, первая для вывода данных, вторая для вывода статистики

--- ISIP523_Zasetsky/Program.cs
+++ ISIP523_Zasetsky/Program.cs
@@ -38,6 +38,88 @@ class Program
         Console.WriteLine($"Макс: {max} руб.");
         Console.WriteLine($"Мин: {min} руб.\n");
     }
+    static void SortCosts(string[] names, decimal[] costs, int n)
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (costs[j] > costs[j + 1])
+                {
+                    (costs[j], costs[j + 1]) = (costs[j + 1], costs[j]);
+                    (names[j], names[j + 1]) = (names[j + 1], names[j]);
+                }
+            }
+        }
+        Console.WriteLine("Данные отсортированы!\n");
+    }
+
+    static void ConvertCurrency(string[] names, decimal[] costs, int n)
+    {
+        Console.WriteLine("Выберите в какую валюту хотите перевести: ");
+        Console.WriteLine("1. Доллар");
+        Console.WriteLine("2. Евро");
+        Console.WriteLine("3. Дирхам");
+        Console.WriteLine("4. Своя валюта");
+
+
+        decimal[] convertedCosts = new decimal[n];
+        for (int i = 0; i < n; i++)
+        {
+            convertedCosts[i] = costs[i];
+        }
+
+        int vibor = Convert.ToInt32(Console.ReadLine());
+        string currencyName = "";
+        decimal usd = 83.0m;
+        decimal eur = 97.0m;
+        decimal dirham = 22.56m;
+        decimal customRate;
+
+
+
+        if (vibor == 1)
+        {
+            currencyName = "доллар";
+            for (int i = 0; i < n; i++)
+            {
+                convertedCosts[i] = convertedCosts[i] / usd;
+                Console.WriteLine($"{names[i]} - {convertedCosts[i]} usd"); // ДОДЕЛАТЬ
+            }
+        }
+        if (vibor == 2)
+        {
+            currencyName = "евро";
+            for (int i = 0; i < n; i++)
+            {
+                convertedCosts[i] = convertedCosts[i] / eur;
+                Console.WriteLine($"{names[i]} - {convertedCosts[i]} eur");
+            }
+        }
+        if (vibor == 3)
+        {
+            currencyName = "дирхам";
+            for (int i = 0; i < n; i++)
+            {
+                convertedCosts[i] = convertedCosts[i] / dirham;
+                Console.WriteLine($"{names[i]} - {convertedCosts[i]} dirham");
+            }
+        }
+        if (vibor == 4)
+        {
+            Console.Write("Введите курс (1 рубль = ?): ");
+            customRate = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Введите название валюты: ");
+            currencyName = Console.ReadLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                convertedCosts[i] = convertedCosts[i] / customRate;
+                Console.WriteLine($"{names[i]} - {convertedCosts[i]} {currencyName}");
+            }
+        }
+    }
+
     static void Main()
     {
         int n = 0;
@@ -85,11 +167,11 @@ class Program
                         ShowStats(costs, n);
                         break;
                     case "3":
-                        SortCosts(); 
-                        ShowData();
+                        SortCosts(names, costs, n); 
+                        ShowData(names, costs, n);
                         break;
                     case "4":
-                        ConvertCurrency();
+                        ConvertCurrency(names, costs, n);
                         break;
                     case "5":
                         SearchName();
