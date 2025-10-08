20e0580baed21e2c48adaf977f9de0e716deb2f9
Reflex414 <9629970659rus@mail.ru>
2025-10-08 15:24:33 +0300

    Написал класс Product, в который добавил функцию для вывода инф-ы

--- ISIP523_Zasetsky/Program.cs
+++ ISIP523_Zasetsky/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StoreManagement
+{
+    public enum ProductCategory
+    {
+        Electronics,
+        Clothing,
+        Books,
+        Food,
+        Sports
+    }
+
+    public class Product
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public bool IsInStock => Quantity > 0;
+        public ProductCategory Category { get; set; }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Код: {Code} | Название: {Name} | Цена: {Price:C}");
+            Console.WriteLine($"Количество: {Quantity} | В наличии: {(IsInStock ? "Да" : "Нет")} | Категория: {Category}");
+            Console.WriteLine(new string('-', 50));
+        }
+    }
+
+    
