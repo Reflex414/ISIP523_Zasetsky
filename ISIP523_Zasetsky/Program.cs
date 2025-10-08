b5362124e7fe0d1961a5f752466658fcaad7f432
Reflex414 <9629970659rus@mail.ru>
2025-10-08 16:51:53 +0300

    Дальше написал ф-ии для поиска самого длинного слова, подсчёта предложений, подсчёта гласных и согласных, статистика букв

--- ISIP523_Zasetsky/Program.cs
+++ ISIP523_Zasetsky/Program.cs
@@ -78,5 +78,107 @@ class Program
                 }
             }
         }
+        void LongestWordSearch()
+        {
+            char[] separators = { ' ', ',', '.', '!', '?', ';', ':', '\t', '\n', '\r', '(', ')', '[', ']', '{', '}', '"', '\'' };
+            string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            if (words.Length == 0)
+            {
+                longestWord = "";
+                return;
+            }
+
+            longestWord = words[0];
+            for (int i = 1; i < words.Length; i++)
+            {
+                if (words[i].Length > longestWord.Length)
+                {
+                    longestWord = words[i];
+                }
+            }
+        }
+
+        void SentencesCount()
+        {
+            sentencesCount = 0;
+            foreach (char ch in text)
+            {
+                if (ch == '.' || ch == '!' || ch == '?')
+                {
+                    sentencesCount++;
+                }
+            }
+        }
+
+        void LettersQuantity()
+        {
+            consonant = 0;
+            vowel = 0;
+
+            string allConsonant = "цкнгшщзхъфвпрлджчсмтьбйЦКНГШЩЗХЪФВПРЛДЖЧСМТЬБЙ";
+            string allVowel = "уеыаоэяиюУЕЫАОЭЯИЮ";
+
+            foreach (char ch in text)
+            {
+                if (char.IsLetter(ch))
+                {
+                    bool isConsonant = false;
+                    for (int i = 0; i < allConsonant.Length; i++)
+                    {
+                        if (allConsonant[i] == ch)
+                        {
+                            isConsonant = true;
+                            break;
+                        }
+                    }
+
+
+                    if (isConsonant)
+                    {
+                        consonant++;
+                    }
+                    else
+                    {
+                        bool isVowel = false;
+                        for (int i = 0; i < allVowel.Length; i++)
+                        {
+                            if (allVowel[i] == ch)
+                            {
+                                isVowel = true;
+                                break;
+                            }
+                        }
+
+                        if (isVowel)
+                        {
+                            vowel++;
+                        }
+                    }
+                }
+            }
+        }
+
+        void StatsLetters()
+        {
+            azbyka.Clear();
+            foreach (char ch in text)
+            {
+                if (!char.IsLetter(ch))
+                    continue;
+
+                char lowerChar = char.ToLower(ch);
+
+                if (azbyka.ContainsKey(lowerChar))
+                    azbyka[lowerChar]++;
+                else
+                    azbyka[lowerChar] = 1;
+            }
+        }
+
+
+        
     }
+
+}
 }
